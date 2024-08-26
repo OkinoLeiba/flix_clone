ba057880b15f9005443201d80e15a83562526ee4
Okino Kamali Leiba <okino.leiba@yahoo.com>
2024-08-26 19:31:09 +0100

    {update}: add elements

--- html_head.js
+++ html_head.js
@@ -62,4 +62,55 @@ createHead = () => {
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
     <script type="text/javascript" src="javascript.js" defer></script>
     <script src="https://kit.fontawesome.com/b2ef8ab3ab.js" crossorigin="anonymous"></script>
-</head>
+      </head>
+                                    
+                                    const siteTitle = document.createElement("title");
+                                    siteTitle.textContent = "Flix";
+                                    document.getElementsByName("content-policy").after(siteTitle);
+
+
+                                    const faviconLink = document.createElement("link");
+                                    faviconLink.setAttribute("name", "favicon-link");
+                                    faviconLink.setAttribute("type", "image/x-icon");
+                                    faviconLink.setAttribute("rel", "icon");
+                                    faviconLink.setAttribute("href", "/img/icon/rubber-ducky-cartoon-black.ico");
+                                    document.getElementsByTagName("title").after(faviconLink);
+
+                                    const styleLink = document.createElement("link");
+                                    styleLink.setAttribute("name", "style-link");
+                                    styleLink.setAttribute("type", "text/css");
+                                    styleLink.setAttribute("rel", "stylesheet");
+                                    styleLink.setAttribute("href", "style.css");
+                                    document.getElementsByName("favicon-link").after(styleLink);
+
+                                    
+                                    const fontAwesome = document.createElement("link");
+                                    fontAwesome.setAttribute("name", "style-link");
+                                    fontAwesome.setAttribute("type", "text/css");
+                                    fontAwesome.setAttribute("rel", "stylesheet");
+                                    fontAwesome.setAttribute("href", "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css");
+                                    document.getElementsByTagName("style-link").after(fontAwesome);
+
+                                    const googleIcons = document.createElement("link");
+                                    googleIcons.setAttribute("name", "google-icons");
+                                    googleIcons.setAttribute("type", "text/css");
+                                    googleIcons.setAttribute("rel", "stylesheet");
+                                    googleIcons.setAttribute("href", "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css");
+                                    document.getElementsByTagName("style-link").after(googleIcons);
+
+                                    const mainScript = document.createElement("script");
+                                    mainScript.setAttribute("name", "main-script);
+                                    mainScript.setAttribute("type", "text/javascript");
+                                    googleIcons.setAttribute("src", "javascript.js");
+                                    googleIcons.setAttribute("defer");
+                                    document.getElementsByTagName("google-icons").after(mainScript);
+
+                                    const fontAwesomeScript = document.createElement("script");
+                                    fontAwesomeScript.setAttribute("name", "font-awesome-script);
+                                    fontAwesomeScript.setAttribute("type", "text/javascript");
+                                    fontAwesomeScript.setAttribute("src", "https://kit.fontawesome.com/b2ef8ab3ab.js");
+                                    fontAwesomeScript.setAttribute("crossorigin", "anonymous");
+                                    document.getElementsByTagName("main-script").after(fontAwesomeScript);
+
+
+                            

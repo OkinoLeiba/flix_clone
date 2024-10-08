ecec0f13b34cb93f8e499a876ef0b94d17bf847e
Okino Kamali Leiba <okino.leiba@yahoo.com>
2024-10-08 16:23:06 +0100

    {update}: add more css properties

--- style.js
+++ style.js
@@ -1,7 +1,29 @@
+/**
+ * @name StyleFunc
+ * @description Function to assign css properties to html targets
+ * @author Okino Kamali Leiba
+ * @function
+ * @returns {void} 
+ */
 
-(function styleClass() {
+/* 
+    Created on : June 24, 2024, 3:21:44 PM
+    Author     : Okino Kamali Leiba
+*/
 
+const root = {
+    gridRepeat: "100",
+    shadowColor: "#a9a9a963",
+    titleColor: "#d9b9b984",
+}
 
+(function styleFunc() {
+    
+
+/* ==========================================================================
+   Global Styles
+   ========================================================================== */
+/* TODO: consider creating default properties for global styles */
     document.querySelector("html").style.cssText = `
         background-color: black !important;
         box-sizing: border-box;
@@ -20,6 +42,14 @@
         overflow-x: hidden;
         overflow-y: scroll;
         scroll-snap-type: y mandatory;`;
+
+/* ==========================================================================
+   Custom Styles
+   ========================================================================== */
+
+/* --------------------------------------------------------------------------
+   Nav Styles
+   -------------------------------------------------------------------------- */    
         
     document.querySelector("#top-nav-icon-image-logo").style.cssText = `
         display: none;
@@ -46,4 +76,91 @@
         border-radius: 7px;
         font-size: 1em;
         text-decoration: none;`;
+    
+    document.querySelector("#top-nav-icon-bar-item-hover").style.cssText = `
+        image-rendering: auto;
+        width: auto;
+        background-color: transparent;
+        float: left;
+        padding: 4px 4px;
+        border: none;`;
+    
+    document.querySelector("#top-nav-search-input").style.cssText = `
+        width: 9.5em;
+        margin: 0px 0px;
+        height: 1.5em;
+        border-color: black;
+        border-radius: 25px;
+        padding: 0 2px;`;
+
+    document.querySelector("#top-nav-onboarding-group").style.cssText = `
+        display: flex;
+        flex-direction: row;
+        margin: 4px 0;
+        padding: 1em;
+        gap: 8px;`;
+    
+    
+    navBGOffSet = `
+        color: root.shadowColor;
+        cursor: pointer;
+        font-size: 1.05rem;
+        line-height: 0.75rem;
+        border-radius: 50px;
+        width: 5.25em;
+        height: 1.5em;`;
+    
+    navBGOffSetAL = `
+    background-color: red;
+    color: black;`;
+
+    // TODO: review and confirm destruct and assignment array
+    document.querySelector("#nav-login-bg-offset").style.cssText = [navBGOffSet, navBGOffSetAL];
+    document.querySelector("#nav-signup-bg-offset").style.cssText = navBGOffSet;
+    document.querySelector("#nav-account-bg-offset").style.cssText = [navBGOffSet, navBGOffSetAL];
+    document.querySelector("#nav-logout-bg-offset").style.cssText = navBGOffSet;
+// Review latter
+//     #nav-signup-bg-offset,
+// #nav-logout-bg-offset {
+//     background-color: transparent;
+// }
+
+// #nav-login-bg-offset,
+// #nav-signup-bg-offset {
+//     display: none;
+// }
+
+// #nav-account-bg-offset,
+// #nav-logout-bg-offset {
+//     display: block;
+    //     }
+    AccLogIcon = ` 
+    width: 20px;
+    height: 20px;
+    padding: 4px 0;
+    /* background-color: transparent; */
+    /* color: black; */
+    /* color-rendering: optimizeQuality; */`;
+
+    document.querySelector("#account-icon").style.cssText = AccLogIcon;
+    document.querySelector("#logout-icon").style.cssText = AccLogIcon;
+
+    document.querySelector("#top-nav-profile-circle").style.cssText = `
+    border-color: black;
+    border-radius: 50px;
+    width: 24px;
+    height: 24px;`;
+
+    document.querySelector("#left-side-panel").style.cssText = `display: none;`;
+    
+
+/* --------------------------------------------------------------------------
+   Banner Hero Styles
+   -------------------------------------------------------------------------- */    
+    
+    
+
+
+
+
 })();
--- style_object.js
+++ style_object.js
@@ -1,6 +1,29 @@
-(function styleClassBody() {
+/**
+ * @name styleFuncBody
+ * @description Function to assign css properties to html targets
+ * @author Okino Kamali Leiba
+ * @function
+ * @returns {void} 
+ */
+
+/* 
+    Created on : June 24, 2024, 3:21:44 PM
+    Author     : Okino Kamali Leiba
+*/
+
+const root = {
+    gridRepeat: "100",
+    shadowColor: "#a9a9a963",
+    titleColor: "#d9b9b984",
+}
+
+(function styleFuncBody() {
 
 
+/* ==========================================================================
+   Global Styles
+   ========================================================================== */
+/* TODO: consider creating default properties for global styles */
     const HTML = document.querySelector("html");
     const htmlStyle = {
         backgroundColor: "black !important",
@@ -19,8 +42,16 @@
         overflowY: "scroll",
         scrollSnapType: "y mandatory",
     };
-    
+
     Object.assign(BODY.style, bodyStyle);
+
+/* ==========================================================================
+   Custom Styles
+   ========================================================================== */
+
+/* --------------------------------------------------------------------------
+   Nav Styles
+   -------------------------------------------------------------------------- */    
 
     const IMAGELOGO = document.querySelector("#top-nav-icon-image-logo");
     const imageLogoStyle = {
@@ -60,4 +91,104 @@
     };
 
     Object.assign(DESKTOPNAV.style, topDesktopNavStyle);
+
+    const NAVICONBATITEM = document.querySelector("#top-nav-icon-bar-item-hover");
+    const topNavIconBarItemStyle = {
+        imageRendering: "auto",
+        width: "auto",
+        backgroundColor: "transparent",
+        float: "left",
+        padding: "4px 4px",
+        border: "none",
+    };
+
+    Object.assign(NAVICONBATITEM.style, topNavIconBarItemStyle);
+
+    const NAVSEARCHINPUT = document.querySelector("#top-nav-search-input");
+    const topNavSearchInputStyle = {
+        width: "9.5em",
+        margin: "0px 0px",
+        height: "1.5em",
+        borderColor: black,
+        borderRadius: "25px",
+        padding: "0 2px",
+    };
+
+    Object.assign(NAVSEARCHINPUT.style, topNavSearchInputStyle);
+
+
+    const NAVONBOARDINGGROUP = document.querySelector("#top-nav-onboarding-group");
+    const topNavOnboardingGroup = {
+        display: "flex",
+        flexDirection: "row",
+        margin: "4px 0",
+        padding: "1em",
+        gap: "8px",
+    };
+
+    Object.assign(NAVONBOARDINGGROUP.style, topNavOnboardingGroup);
+
+
+    const navBGOffSetStyle = {
+        color: root.shadowColor,
+        cursor: "pointer",
+        fontSize: "1.05rem",
+        lineHeight: "0.75rem",
+        borderRadius: "50px",
+        width: "5.25em",
+        height: "1.5em",
+    };
+
+    const navBGOffSetALStyle = {
+        backgroundColor: "red",
+        color: "black",
+    };
+
+    const LOGINBGOFFSET = document.querySelector("#nav-login-bg-offset");
+    const SIGNUPBGOFFSET = document.querySelector("#nav-signup-bg-offset");
+    const ACCOUNTBGOFFSET = document.querySelector("#nav-account-bg-offset");
+    const LOGOUTBGOFFSET = document.querySelector("#nav-logout-bg-offset");
+
+    // TODO: review and confirm if assign function accepts array as argument
+    Object.assign([LOGINBGOFFSET.style, SIGNUPBGOFFSET.style, ACCOUNTBGOFFSET.style, LOGOUTBGOFFSET.style], navBGOffSetStyle);
+
+    Object.assign([LOGINBGOFFSET.style, ACCOUNTBGOFFSET.style], navBGOffSetALStyle);
+
+
+    const AccLogIconStyle = {
+        width: "20px",
+        height: "20px",
+        padding: "4px 0",
+        /* backgroundColor: "transparent", */
+        /* color: "black", */
+        /* colorRendering: "optimizeQuality" */
+    };
+
+    const ACCICON = document.querySelector("#account-icon");
+    const LOGICON = document.querySelector("#logout-icon");
+
+    Object.assign([ACCICON.style, LOGICON.style], AccLogIconStyle);
+
+
+    const PROFILECIRCLE = document.querySelector("#top-nav-profile-circle");
+    const profileCircleStyle = {
+        borderColor: "black",
+        borderRadius: "50px",
+        width: "24px",
+        height: "24px",
+    };   
+
+    Object.assign(PROFILECIRCLE.style, profileCircleStyle)
+
+    const LEFTSIDEPANEL = document.querySelector("#left-side-panel");
+    const leftSidePanelStyle = {
+        display: "none",
+    };
+
+    Object.assign(LEFTSIDEPANEL.style, leftSidePanelStyle);
+
+/* --------------------------------------------------------------------------
+   Banner Hero Styles
+   -------------------------------------------------------------------------- */
+
 })();
